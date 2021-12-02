e3d1bc4abab9c42607ce4e0f92d62ff0de593e62
Armaghan <rasheedarmaghan@gmail.com>
2021-12-02 10:38:28 +0500

    implemented animation
    implemented player flying and walking animation

--- Space Man/Assets/Scripts/Player.cs
+++ Space Man/Assets/Scripts/Player.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float upForce;
+    private bool onGround;
+
+    public Animator playerAnimation;
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerAnimation = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,6 +27,16 @@ public class Player : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
             rb.AddForce(Vector2.up * upForce);
+            playerAnimation.SetBool("Flying", true);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            playerAnimation.SetBool("Flying", false);
+        }
+    }
+
 }
