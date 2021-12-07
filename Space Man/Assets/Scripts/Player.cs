e25d187422650967cb64750efa7f0149351eecab
Armaghan <rasheedarmaghan@gmail.com>
2021-12-07 11:23:15 +0500

    enemy prefab

--- Space Man/Assets/Scripts/Player.cs
+++ Space Man/Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public class Player : GameManager
+public class Player : MonoBehaviour
 {
     public float upForce;
 
