2562748e21a60095fd0aa951a1f114467696e2db
Armaghan <rasheedarmaghan@gmail.com>
2021-12-07 12:07:28 +0500

    object instantiated

--- Space Man/Assets/Scripts/SpawnManager.cs
+++ Space Man/Assets/Scripts/SpawnManager.cs
@@ -5,17 +5,34 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
 
-    public GameObject[] enimies;
+    public GameObject[] enemyPrefab;
+    private float enemySpawnTime=2;  // spawn time for normal enemy
+    private float enemyYRange=2.35f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnAfterInterval());
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+    }
+
+
+    IEnumerator SpawnAfterInterval()
+    {
+        yield return new WaitForSeconds(enemySpawnTime);
+        SpawnNormalEnemy();
+    }
+    void SpawnNormalEnemy()
+    {
+        int randomIndex = Random.Range(0, enemyPrefab.Length); // for choosing the random enemy(planet)
         
+        Vector2 randomPosition = new Vector2(11,Random.Range(-enemyYRange,enemyYRange));
+        
+        Instantiate(enemyPrefab[randomIndex],randomPosition,enemyPrefab[randomIndex].transform.rotation);
     }
 }
